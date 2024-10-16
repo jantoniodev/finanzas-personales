e68fc64cb9f18bb06f7556b56f37f733eb7af362
jantoniodev <jose.lopezh95@gmail.com>
2024-10-16 19:31:12 -0300

    get credit cards id list

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import puppeteer from 'puppeteer'
 
 const BANK_LOGIN_URL = 'https://login.portales.bancochile.cl/login'
+const BANK_PRODUCTS_URL = 'https://portalpersonas.bancochile.cl/mibancochile/rest/persona/bff-ppersonas-prd-selector/productos/obtenerProductos?incluirTarjetas=true'
 const BANK_USER = process.env.BANK_USER
 const BANK_PASSWORD = process.env.BANK_PASSWORD
 
@@ -32,11 +33,29 @@ const getCookies = async () => {
     return formattedCookies
 }
 
+const getCreditCardsIds = async (cookies) => {
+    const response = await fetch(BANK_PRODUCTS_URL, {
+        headers: {
+            'cookie': cookies
+        }
+    })
+
+    const data = await response.json()
+
+    const creditCardsIds = data.productos
+        .filter(product => product.codigo === 'TNM')
+        .map(product => product.id)
+
+    return creditCardsIds
+}
+
 const app = async () => {
     console.log('[+] Obteniendo cookies')
     const cookies = await getCookies()
 
-
+    console.log('[+] Obteniendo tarjetas de crédito')
+    const creditCardsIds = await getCreditCardsIds(cookies)
+    console.log('\t[=] Tarjetas de crédito encontradas:', creditCardsIds.join(', '))
 }
 
 await app()
