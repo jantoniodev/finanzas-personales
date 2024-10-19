6bd9d3b63b24a8f291929f4cc402c806ba8d1254
jantoniodev <jose.lopezh95@gmail.com>
2024-10-18 23:14:10 -0300

    add not billed total

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ import puppeteer from 'puppeteer'
 const BANK_LOGIN_URL = 'https://login.portales.bancochile.cl/login'
 const BANK_PRODUCTS_URL = 'https://portalpersonas.bancochile.cl/mibancochile/rest/persona/bff-ppersonas-prd-selector/productos/obtenerProductos?incluirTarjetas=true'
 const BANK_BILLING_DATES = 'https://portalpersonas.bancochile.cl/mibancochile/rest/persona/tarjetas/estadocuenta/fechas-facturacion'
+const BANK_NOT_BILLED_MOVEMENTS = 'https://portalpersonas.bancochile.cl/mibancochile/rest/persona/tarjeta-credito-digital/movimientos-no-facturados'
+const BANK_USD_PRICE = 'https://portalpersonas.bancochile.cl/mibancochile/rest/persona/home/indices-financieros'
 const BANK_USER = process.env.BANK_USER
 const BANK_PASSWORD = process.env.BANK_PASSWORD
 
@@ -75,17 +77,81 @@ const getBillingDates = async (cookies, cardId) => {
     }
 }
 
+const getNotBilledMovements = async (cookies, cardId) => {
+    const response = await fetch(BANK_NOT_BILLED_MOVEMENTS, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'cookie': cookies
+        },
+        body: JSON.stringify({
+            idTarjeta: cardId
+        })
+    })
+
+    const data = await response.json()
+
+    const moreThanTwoSpacesRegex = /\s{2,}/g
+
+    return data.listaMovNoFactur
+        .filter(movement => movement.montoCompra >= 0)
+        .map(movement => {
+            return {
+                type: movement.origenTransaccion === 'NAC' ? 'national' : 'international',
+                amount: movement.montoCompra,
+                date: movement.fechaTransaccion,
+                description: movement.glosaTransaccion.replaceAll(moreThanTwoSpacesRegex, ' '),
+                installment: parseInt(movement.numeroCuotas),
+                totalInstallments: parseInt(movement.numeroTotalCuotas === '0' ? '1' : movement.numeroTotalCuotas),
+            }
+        })
+}
+
+const getUsdPrice = async (cookies) => {
+    const response = await fetch(BANK_USD_PRICE, {
+        headers: {
+            'cookie': cookies
+        }
+    })
+
+    const data = await response.json()
+
+    return data.datosObservados
+
+}
+
+const calculateTotalBilledAmount = (billedMovements, usdPrice) => {
+    return billedMovements
+        .reduce((total, movement) => 
+            movement.type === 'national'
+            ? total + (movement.amount / movement.totalInstallments)
+            : total + (movement.amount / movement.totalInstallments * usdPrice)
+        , 0)
+}
+
+const formatAmount = (amount) => {
+    return new Intl.NumberFormat('es-CL', { style: 'currency', currency: 'CLP' }).format(amount)
+}
+
 const app = async () => {
     console.log('[+] Obteniendo cookies')
     const cookies = await getCookies()
+
+    const usdPrice = await getUsdPrice(cookies)
+    console.log('[+] Precio del dólar:', usdPrice)
 
     console.log('[+] Obteniendo tarjetas de crédito')
     const creditCardsIds = await getCreditCardsIds(cookies)
     console.log('\t[=] Tarjetas de crédito encontradas:', creditCardsIds.join(', '))
 
     for (const cardId of creditCardsIds) {
-        console.log(`[+] Obteniendo fechas de facturación para la tarjeta ${cardId}`)
-        const billingDates = await getBillingDates(cookies, cardId)
+        console.log(`[+] Tarjeta de crédito: ${cardId}`)
+
+        console.log(`\t[+] Obteniendo movimientos no facturados`)
+        const notBilledMovements = await getNotBilledMovements(cookies, cardId)
+
+        const totalBilledAmount = calculateTotalBilledAmount(notBilledMovements, usdPrice)
+        console.log(`\t[=] Total movimientos no facturados: ${formatAmount(totalBilledAmount)}`)
     }
 }
 
